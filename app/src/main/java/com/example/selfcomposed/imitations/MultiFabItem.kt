e9a241059489716895ac3d06d6fec67409b19492
HoinzeyBear <darrenhoyne@gmail.com>
2022-08-30 22:56:33 +0100

    playing around with multi button fab

--- app/src/main/java/com/example/selfcomposed/imitations/MultiFabItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.selfcomposed.imitations
-
-import androidx.compose.ui.graphics.ImageBitmap
-
-class MultiFabItem(
-    val identifier: String,
-//    val icon: ImageBitmap,
-    val label: String
-)
--- app/src/main/java/com/example/selfcomposed/imitations/Twitter.kt
+++ app/src/main/java/com/example/selfcomposed/imitations/Twitter.kt
@@ -4,28 +4,36 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.selfcomposed.ui.theme.ButtonShadow
+import com.example.selfcomposed.ui.theme.TwitterBlue
 
 @Composable
 fun Twitter() {
@@ -42,11 +50,12 @@ fun MainContainer(modifier: Modifier) {
     Box(modifier = modifier) {
 
         var fabState by remember { mutableStateOf(MultiFabState.COLLAPSED) }
+        Text(text = "Sample")
         if(fabState == MultiFabState.EXPANDED) {
             OpacityScreen(modifier = Modifier
                 .fillMaxSize()
-                .alpha(0.3F)
-                .background(color = Color.Blue))
+                .alpha(0.9F)
+                .background(color = Color.White))
         }
 
         ActionButton(
@@ -85,18 +94,15 @@ fun ActionButton(
         }
 
         if (transition.currentState == MultiFabState.EXPANDED) {
-            MiniFloatingActionButton(MultiFabItem("stuff",
-//                icon = bitmap!!,
-                label = "Spaces"), onFabItemClicked =  {}, buttonScale = scale, iconAlpha = alpha)
-            Spacer(modifier = Modifier.height(10.dp))
-            MiniFloatingActionButton(MultiFabItem("stuff",
-//                icon = bitmap!!,
-                label = "Photo"), onFabItemClicked =  {}, buttonScale = scale, iconAlpha = alpha)
-            Spacer(modifier = Modifier.height(10.dp))
-            MiniFloatingActionButton(MultiFabItem("stuff",
-//                icon = bitmap!!,
-                label = "Gif"), onFabItemClicked =  {}, buttonScale = scale, iconAlpha = alpha)
-            Spacer(modifier = Modifier.height(10.dp))
+
+            MultiFabItems.values().forEach {
+                MiniFloatingActionButton(
+                    item = it,
+                    buttonScale = scale,
+                    iconAlpha = alpha,
+                    onFabItemClicked = {})
+                Spacer(modifier = Modifier.height(10.dp))
+            }
         }
 
         FloatingActionButton(
@@ -109,12 +115,13 @@ fun ActionButton(
                         MultiFabState.COLLAPSED
                     } else MultiFabState.EXPANDED)
             },
-            backgroundColor = Color.Green) {
+            backgroundColor = TwitterBlue) {
 
             Icon(
                 modifier = Modifier.size(40.dp),
                 imageVector = Icons.Default.Add,
-                contentDescription = null)
+                contentDescription = null,
+                tint = Color.White)
         }
     }
 
@@ -122,31 +129,56 @@ fun ActionButton(
 
 @Composable
 private fun MiniFloatingActionButton(
-    item: MultiFabItem,
+    item: MultiFabItems,
     buttonScale: Float,
     iconAlpha: Float,
-    onFabItemClicked: (item: MultiFabItem) -> Unit
+    onFabItemClicked: (item: MultiFabItems) -> Unit
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(end = 21.dp)
+        modifier = Modifier.padding(end = 20.dp)
     ) {
         Text(
             item.label,
-            fontSize = 12.sp,
-            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
             modifier = Modifier
                 .alpha(animateFloatAsState(iconAlpha).value)
-                .background(color = MaterialTheme.colors.surface)
-                .padding(start = 6.dp, end = 6.dp, top = 4.dp)
+                .padding(start = 2.dp, end = 3.dp, top = 4.dp)
         )
         Spacer(modifier = Modifier.width(16.dp))
-        IconButton(
-            modifier = Modifier
-                .clip(CircleShape)
-                .background(color = Color.LightGray),
-            onClick = {  }) {
+
+        Surface(
+            shape = CircleShape,
+            border = BorderStroke(width = 1.dp, color = ButtonShadow),
+            modifier = Modifier.size(40.dp)
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(color = Color.White),
+                onClick = { onFabItemClicked(item) }) {
+
+                Icon(
+                    modifier = Modifier.size(25.dp),
+                    imageVector =item.image,
+                    contentDescription = null,
+                    tint = TwitterBlue)
+            }
         }
+//        IconButton(
+//            modifier = Modifier
+//                .clip(CircleShape)
+//                .background(color = Color.White)
+//                .border(width = 2.dp, color = TwitterBlue),
+//            onClick = { onFabItemClicked(item) }) {
+//
+//            Icon(
+//                modifier = Modifier.size(30.dp),
+//                imageVector =item.image,
+//                contentDescription = null,
+//                tint = TwitterBlue)
+//        }
     }
 }
 
@@ -159,42 +191,20 @@ fun PreviewFabStack() {
             fabState = MultiFabState.EXPANDED)
     }
 }
-/*
-@Preview(name = "Elevation")
-@Composable
-fun Elevations() {
-    Row(modifier = Modifier
-        .background(MaterialTheme.colors.surface)
-        .padding(5.dp),
-        horizontalArrangement = Arrangement.Start) {
-
-        Surface(shape = RoundedCornerShape(12.dp)){
-            Text(text = "Text 1", modifier = Modifier
-                .background(OrangeHighlight)
-                .padding(8.dp), color = BlogGreen)
-        }
-
-        Surface(
-            modifier = Modifier.padding(start = 4.dp),
-            shape = RoundedCornerShape(12.dp),
-            elevation = 12.dp){
-            Text(text = "Text 2", modifier = Modifier
-                .padding(8.dp), color = BlogGreen)
-        }
-
-        Surface(Modifier.padding(start = 4.dp), shape = RoundedCornerShape(12.dp)){
-            Text(text = "Text 3", modifier = Modifier
-                .background(PurplyBlueContrast)
-                .padding(8.dp), color = BlogGreen)
-        }
-    }
-}
- */
 
 
 enum class MultiFabState {
     COLLAPSED, EXPANDED
 }
 
+enum class MultiFabItems(
+    val identifier: String,
+    val image: ImageVector,
+    val label: String) {
+
+    SPACES("SPACES", Icons.Outlined.Info,"Spaces"),
+    PHOTO("PHOTO", Icons.Outlined.Phone, "Photo"),
+    GIF("GIF", Icons.Outlined.Build, "Gif")
+}
 
 
